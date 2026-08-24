b91f8566596c32186c4e3a2c1ae4898fa5c8e83f
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-08-25 00:40:14 +0700

    refactor(persistence): drop sqlite export

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -4,9 +4,8 @@ export * from './schema.js';
 export * from './hash.js';
 export * from './memory.js';
 export * from './file.js';
-export * from './sqlite.js';
 export * from './postgres.js';
 export * from './factory.js';
 export * from './file-backup.js';
 
-export * from "./client.js";
+export * from './client.js';
